750cfbca79571d26bfa003e74e23edd5e1b9ce49
Kyle Michaels <kyle.michaels@gmail.com>
2024-05-14 12:49:40 -0700

    Created Get All Movies endpoint

--- index.js
+++ index.js
@@ -28,6 +28,28 @@ app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
 
+/**
+ * Handles Get request for all movies.
+ * 
+ * @function
+ * @name getAllMovies
+ * @param {Object} - Express request.
+ * @param {Object} - Express response.
+ * @returns {Promise<void>} - A promise that resolves when getAllMovies request is complete.
+ * @throws {Error} - If permission is denied or unexpected error.
+ * @returns {Object} []allMovies - An array of the movies collection.
+ */
+app.get('/movies', async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    })
+})
+
 
 // Listener
 app.listen(8080, () => {
