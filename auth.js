4c486184c6606be0a4b2ff6f613091f9e77be957
Kyle Michaels <kyle.michaels@gmail.com>
2024-05-14 15:24:22 -0700

    Created generateJWTToken

---
+++ auth.js
@@ -0,0 +1,14 @@
+const jwtSecret = process.env.JWT_SECRET;
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+require('./passport');
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username,
+    expiresIn: '7d',
+    algorithm: 'HS256'
+  });
+}
