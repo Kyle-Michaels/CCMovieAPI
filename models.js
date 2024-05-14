ff7552c06112fcd3e9182eeb13a2f3c2eb09ef09
Kyle Michaels <kyle.michaels@gmail.com>
2024-05-14 14:53:04 -0700

    Created movie schema.

--- models.js
+++ models.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
